5f37b3bf38e6269f6a23d22cb80c616ec7495ef1
Gastton <gastonsilva13@gmail.com>
2021-04-12 17:42:22 -0300

    changes

--- csv_exercise.py
+++ csv_exercise.py
@@ -10,7 +10,16 @@ for row in csv_f:
     print ("City:  {}, State: {}".format(City, State))
 f.close()
 """
+"""
 host = [["workstation.local","192.102.33.251"],["webserver.cloud", "33.102.231.1"]]
 with open(curPath + "host_file.csv","w") as host_csv:
     writer = csv.writer(host_csv)
     writer.writerows(host)
+"""
+
+f = open(curPath + "cities.csv", "r")
+csv_f = csv.DictReader(f)
+for row in csv_f:
+    LatD, LatM, LatS, NS, LonD, LonM, LonS, EW, City, State = row
+    print ("City:  {}, State: {}".format(City, State))
+f.close()
